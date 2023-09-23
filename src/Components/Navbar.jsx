e47660e618945c2080e7bc41836d0b89ecc910e3
SajeedAninda <sajaninda@gmail.com>
2023-09-23 12:08:22 +0600

    Made Navbar Responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,9 +4,9 @@ import { NavLink, Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
-            <nav className='py-6 flex justify-between items-center'>
+            <nav className='py-6 flex flex-col gap-4 lg:gap-0 lg:flex-row justify-between items-center'>
                 <div>
-                    <Link to={`/`}><h1 className='text-[#1A1919] font-extrabold text-3xl'>Career<span className='text-[#85D7A9]'>Navigator</span></h1></Link>
+                    <Link to={`/`}><h1 className='text-[#1A1919] font-extrabold text-xl md:text-2xl lg:text-3xl'>Career<span className='text-[#85D7A9]'>Navigator</span></h1></Link>
                 </div>
                 <div className='flex gap-6 text-lg'>
                     <NavLink
