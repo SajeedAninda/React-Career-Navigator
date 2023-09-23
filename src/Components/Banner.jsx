4cfc5c2c77aa2c21e9cf87c1e024ab0716e91a67
SajeedAninda <sajaninda@gmail.com>
2023-09-23 12:14:26 +0600

    Made a responsive banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,10 +3,10 @@ import user from '../assets/images/user.png'
 
 const Banner = () => {
     return (
-        <div className='flex justify-between items-center gap-20'>
-            <div className='space-y-5'>
-                <h1 className='text-6xl leading-20 font-bold text-[#1A1919]'>One Step <br /> Closer To Your <br /> <span className='text-[#85D7A9]'>Dream Job</span></h1>
-                <p className='text-[#757575]'>
+        <div className='flex justify-between items-center flex-col-reverse md:flex-row gap-6 md:gap-8 lg:gap-20'>
+            <div className='space-y-5 text-center md:text-left'>
+                <h1 className='md:text-4xl lg:text-6xl leading-20 font-bold text-[#1A1919]'>One Step <br /> Closer To Your <br /> <span className='text-[#85D7A9]'>Dream Job</span></h1>
+                <p className='text-[#757575] text-sm md:text-sm lg:text-base'>
                     Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. <br /> Manage all your job application from start to finish.
                 </p>
                 <button className='px-4 py-3 rounded-lg font-bold bg-[#85D7A9] text-white'>Get Started</button>
